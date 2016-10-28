33cc16b8d7b1275913172eb334d1ad0ae4762d02
jakabadambalazs <jakabadambalazs@gmail.com>
2016-10-28 16:57:57 +0200

    js inclusion ok

--- themes/agora/src/Agora/Hook/Alter/Javascript.php
+++ themes/agora/src/Agora/Hook/Alter/Javascript.php
@@ -9,6 +9,7 @@ namespace Agora\Hook\Alter;
 
 use Agora\Hook\Hook;
 use Agora\Hook\HookInterface;
+use Symfony\Component\Yaml\Parser;
 
 /**
  * Class Javascript
@@ -17,15 +18,8 @@ use Agora\Hook\HookInterface;
  */
 class Javascript extends Hook implements HookInterface
 {
-    /** @var array */
-    private static $keep = [
-        'settings',
-        'misc/drupal.js',
-        'misc/jquery.js',
-        /*'misc/jquery.once.js',*/
-        /*'sites/all/modules/contrib/devel/devel_krumo_path.js',*/
-        'sites/all/modules/custom/kadmin/js/kadmin.js',
-    ];
+    /** @var  Parser */
+    private static $parser;
     
     /**
      * @param array $js
@@ -33,19 +27,103 @@ class Javascript extends Hook implements HookInterface
     public static function execute(&$js)
     {
         self::excludeAllNonAgoraJavascripts($js);
+        self::includeJsForAgora($js);
+    }
+    
+    
+    /**
+     * loads yaml file corresponding to the type|alias of current node
+     *
+     *
+     */
+    private static function includeJsForAgora(&$js)
+    {
+        $themePath = drupal_get_path('theme', $GLOBALS['theme']);
+        $baseConfigDir = $themePath . '/config/js/';
+        /** @var \stdClass $menuItem */
+        $menuItem = menu_get_object();
+        
+        $nodeType = isset($menuItem->type) ? $menuItem->type : false;
+        $nodeId = isset($menuItem->nid) ? $menuItem->nid : false;
+        
+        // Site wide config
+        $config = self::getParsedConfig($baseConfigDir . 'site.yml');
+        $js = array_merge($js, $config);
+
+        //Content type config
+        if($nodeType)
+        {
+            $config = self::getParsedConfig($baseConfigDir . 'nodetype/' . $nodeType . '.yml');
+            $js = array_merge($js, $config);
+        }
+    
+        //Content Id config
+        if($nodeId)
+        {
+            $config = self::getParsedConfig($baseConfigDir . 'nodeid/' . $nodeId . '.yml');
+            $js = array_merge($js, $config);
+        }
+        
+        //dpm($js, "JS(FINAL)");
     }
     
     /**
+     * @param string $path
+     *
+     * @return array
+     */
+    private static function getParsedConfig($path)
+    {
+        $answer = [];
+        if(is_file($path))
+        {
+            self::$parser = self::$parser ? self::$parser : new Parser();
+            $config = self::$parser->parse(file_get_contents($path));
+            if (is_array($config) && isset($config["js"])) {
+                $answer = $config["js"];
+                $defaultElement = self::getDefaultJsElement();
+                foreach($answer as $filepath => &$fileConfig)
+                {
+                    $fileConfig = array_merge($defaultElement, $fileConfig);
+                    $fileConfig['data'] = $filepath;
+                }
+            }
+        }
+        
+        return $answer;
+    }
+    
+    /**
+     * @see: includes/common.inc:4240
+     *
+     * @return array
+     */
+    private static function getDefaultJsElement()
+    {
+        return [
+            'data' => '',
+            'type' => 'file',
+            'scope' => 'footer',
+            'group' => JS_LIBRARY,
+            'every_page' => FALSE,
+            'weight' => -1,
+            'requires_jquery' => TRUE,
+            'preprocess' => TRUE,
+            'cache' => TRUE,
+            'defer' => FALSE,
+        ];
+    }
+    
+    /**
+     * Tear away all js
      * @param array $js
      */
     private static function excludeAllNonAgoraJavascripts(&$js)
     {
-        $themePath = drupal_get_path('theme', $GLOBALS['theme']);
-        
         //dpm($js, "JS(BEFORE)");
         foreach ($js as $k => $v)
         {
-            if (!(in_array($k, self::$keep) || preg_match('#^' . $themePath . '#', $k)))
+            if ($k != 'settings')
             {
                 unset($js[$k]);
             }
