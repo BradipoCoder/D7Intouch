058c30f38f5bab4e43a6ddd61105ee6a99b7f336
jakabadambalazs <jakabadambalazs@gmail.com>
2016-11-07 10:05:42 +0100

    remote sync

--- themes/agora/templates/paragraphs/video/paragraphs-item--video.tpl.php
+++ themes/agora/templates/paragraphs/video/paragraphs-item--video.tpl.php
@@ -9,7 +9,8 @@
 ?>
 
 <div class="<?php print $classes; ?>">
-    <div class="video-container">
-        <?php print render($content["field_video"]); ?>
+    <div class="video-container embed-responsive embed-responsive-16by9">
+        <?php print trim(render($content["field_video"]),"\r\n "); ?>
+        <!--<div data-type="youtube" data-video-id="<?php print render($content["field_video"]); ?>"></div>-->
     </div>
 </div>
