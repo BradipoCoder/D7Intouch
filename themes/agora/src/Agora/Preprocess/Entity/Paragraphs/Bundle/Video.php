7dd83bcd06367fc9df35752e53775a7dc3bda04f
jakabadambalazs <jakabadambalazs@gmail.com>
2016-11-07 10:36:24 +0100

    clean video url

---
+++ themes/agora/src/Agora/Preprocess/Entity/Paragraphs/Bundle/Video.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by Adam Jakab.
+ * Date: 02/11/16
+ * Time: 16.34
+ */
+
+namespace Agora\Preprocess\Entity\Paragraphs\Bundle;
+
+use Mekit\Drupal7\HookInterface;
+
+class Video implements HookInterface
+{
+    /**
+     * @param array $vars
+     */
+    public static function execute(&$vars)
+    {
+        self::addCleanVideoUrlToContent($vars);
+        dpm($vars, "VIDEO");
+    }
+    
+    private static function addCleanVideoUrlToContent(&$vars)
+    {
+        $cleanVideoUrl = '';
+        if(isset($vars["field_video"][0]["video_url"]))
+        {
+            $cleanVideoUrl = $vars["field_video"][0]["video_url"];
+        }
+        $vars["content"]["clean_video_url"] = [
+            '#markup' => $cleanVideoUrl,
+        ];
+    }
+    
+}
--- themes/agora/templates/paragraphs/video/paragraphs-item--video.tpl.php
+++ themes/agora/templates/paragraphs/video/paragraphs-item--video.tpl.php
@@ -6,11 +6,14 @@
  * Context:     any
  * View mode:   any
  */
+
+//added custom $content["clean_video_url"]
+
 ?>
 
 <div class="<?php print $classes; ?>">
     <div class="video-container embed-responsive embed-responsive-16by9">
-        <?php print trim(render($content["field_video"]),"\r\n "); ?>
-        <!--<div data-type="youtube" data-video-id="<?php print render($content["field_video"]); ?>"></div>-->
+        <pre>clean video url: "<?php print render($content["clean_video_url"]); ?>"</pre>
+        <div data-type="youtube" data-video-id="<?php print render($content["clean_video_url"]); ?>"></div>
     </div>
 </div>
