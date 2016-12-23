af2b1edc3cbcd78c8f22f96038528c0916c68d7f
jakabadambalazs <jakabadambalazs@gmail.com>
2016-12-23 15:02:59 +0100

    video fix - caption

--- themes/agora/templates/paragraphs/video/paragraphs-item--video--news--full.tpl.php
+++ themes/agora/templates/paragraphs/video/paragraphs-item--video--news--full.tpl.php
@@ -12,10 +12,16 @@
 ?>
 
 <div class="media-container">
-    <div class="caption-container">
-        <div class="caption-icon-container"><i class="dida-icons videos"></i></div>
-        <div class="article-gallery-caption-text minore"><?php print render($content["video_description"]); ?></div>
-    </div>
+    <?php if(!empty($content["video_description"]["#markup"])) :?>
+        <div class="caption-container">
+            <div class="caption-icon-container">
+                <i class="dida-icons videos"></i>
+            </div>
+            <div class="article-gallery-caption-text minore">
+                <?php print render($content["video_description"]); ?>
+            </div>
+        </div>
+    <?php endif; ?>
     <div class="embed-responsive embed-responsive-16by9">
         <?php print render($content["player"]); ?>
     </div>
