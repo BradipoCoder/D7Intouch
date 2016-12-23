ab1a09165be9eeb28a7da4df0bb135f9fb6c5af9
jakabadambalazs <jakabadambalazs@gmail.com>
2016-12-23 14:34:14 +0100

    gallery fix - caption

--- themes/agora/templates/paragraphs/imagegallery/paragraphs-item--imagegallery--style-single.tpl.php
+++ themes/agora/templates/paragraphs/imagegallery/paragraphs-item--imagegallery--style-single.tpl.php
@@ -14,12 +14,14 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="article-gallery-content">
-                    <div class="caption-container">
-                        <div class="caption-icon-container"><i class="dida-icons quotes"></i></div>
-                        <div class="article-gallery-caption-text minore">
-                            <?php print render($content["image_gallery"]["caption"]); ?>
+                    <?php if(!empty($content["image_gallery"]["caption"]["#markup"])) :?>
+                        <div class="caption-container">
+                            <div class="caption-icon-container"><i class="dida-icons quotes"></i></div>
+                            <div class="article-gallery-caption-text minore">
+                                <?php print render($content["image_gallery"]["caption"]); ?>
+                            </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                     <div class="article-gallery-img-container embed-responsive embed-responsive-16by9">
                         <?php print render($content["image_gallery"]["image"]); ?>
                     </div>
