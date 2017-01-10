454cd6ef85819e6362fd674873bca2c040fabf17
jakabadambalazs <jakabadambalazs@gmail.com>
2017-01-10 15:45:26 +0100

    front clean up

--- themes/agora/src/Agora/Preprocess/Front.php
+++ themes/agora/src/Agora/Preprocess/Front.php
@@ -23,47 +23,7 @@ class Front implements HookInterface
     public static function execute(&$vars)
     {
         self::killNoContentSystemMessage($vars);
-        self::addViewsToContent($vars);
-        
         //dpm($vars['page']['content'], "FRONT");
-    }
-    
-    
-    /**
-     * @param array $vars
-     */
-    private static function addViewsToContent(&$vars)
-    {
-        $content = [
-            '#prefix' => '<div class="latest-issues-container">',
-            '#suffix' => '</div>',
-            'latest-issue' => [
-                '#markup' => ThemeHelper::getView("front_latest_issue", "block"),
-            ],
-            'past-issue' => [
-                '#markup' => ThemeHelper::getView("front_past_issue", "block"),
-            ],
-            'past-issues' => [
-                '#prefix' => '<section class="past-issues-container dark-bg js-to-show"><div class="container">',
-                '#suffix' => '</div></section>',
-                'header' => [
-                    '#prefix' => '<header class="past-issues-header clearfix">',
-                    '#suffix' => '</header>',
-                    '#markup' => '<h3 class="pull-left">#Past Issues</h3>'
-                        . '<a href="#" class="pull-right btn btn-outline btn-outline--white btn-inner-icon visible-reg-on">'
-                        . '<span>See all</span>'
-                        . '<img src="'.AGORAPATH.'/images/icons/arrow-right__white.svg">'
-                        . '</a>'
-                ],
-                'issues' => [
-                    '#prefix' => '<div class="past-issues-body"><div class="row">',
-                    '#suffix' => '</div></div>',
-                    '#markup' => ThemeHelper::getView("front_3_past_issues", "block"),
-                ],
-            ],
-        ];
-        
-        $vars['page']['content']['main'] = $content;
     }
     
     /**
--- themes/agora/src/Agora/Preprocess/Page.php
+++ themes/agora/src/Agora/Preprocess/Page.php
@@ -36,50 +36,12 @@ class Page implements HookInterface
      */
     private static function generateContentSpecificNavbar(&$vars)
     {
-        $hasNavigation = false;
-        if (isset($vars["node"]))
-        {
-            /** @var \stdClass $node */
-            $node = $vars["node"];
-            if (in_array($node->type, ["standard", "long", "event"]))
-            {
-                self::generateArticleNavbar($vars);
-                $hasNavigation = true;
-            }
-        }
-        
         if (ThemeHelper::getCurrentArea() == ThemeHelper::AREA_INTOUCH)
         {
             self::generateNewsletterArticleNavbar($vars);
-            $hasNavigation = true;
-        }
-        
-        //fallback to main navigation
-        if (!$hasNavigation)
-        {
-            self::generateMainNavbar($vars);
         }
     }
     
-    /**
-     * Generic navigation bar for site
-     *
-     * @param array $vars
-     */
-    private static function generateMainNavbar(&$vars)
-    {
-        
-        $agoraNavGen = [
-            '#theme'    => 'agoranav_generic',
-            '#pagevars' => null,
-            '#settings' => [
-                'latest-articles-markup' => ThemeHelper::getView("navigation_latest_news", "block"),
-                'latest-issues-markup'   => ThemeHelper::getView("navigation_last_issues", "block"),
-            ],
-        ];
-        
-        $vars['page']['main_header']['agora-main-navbar'] = $agoraNavGen;
-    }
     
     /**
      * Header side-bar for: newsletter / newsletter article / topic
@@ -160,94 +122,6 @@ class Page implements HookInterface
         $vars['page']['content']['sidebar'] = $agoraNavBar;
     }
     
-    /**
-     * Navigation bar for single article nodes
-     *
-     * @param array $vars
-     */
-    private static function generateArticleNavbar(&$vars)
-    {
-        if (isset($vars["node"]))
-        {
-            /** @var \stdClass $node */
-            $node = $vars["node"];
-            
-            if (in_array($node->type, ["standard", "long", "event"]))
-            {
-                //dpm($node, "PAGE NODE");
-                
-                $customlink1 = false;
-                
-                //Event has special google calendar link
-                if ($node->type == "event")
-                {
-                    //dpm($node, "N");
-                    $customlink1 = "#";
-                    if (isset($node->field_event_date[LANGUAGE_NONE][0]['value'])
-                        && isset
-                        ($node->field_event_date[LANGUAGE_NONE][0]['value2'])
-                    )
-                    {
-                        // Title
-                        $title = $node->title;
-                        
-                        // Details
-                        $details = '';
-                        
-                        // Location
-                        $location = "";
-                        if (isset($node->field_event_location['und'][0]['value']))
-                        {
-                            $location = $node->field_event_location['und'][0]['value'];
-                        }
-                        
-                        // Dates
-                        $timeZone = new \DateTimeZone($node->field_event_date[LANGUAGE_NONE][0]['timezone']);
-                        //$startDate = new \DateTime($node->field_event_date[LANGUAGE_NONE][0]['value'], $timeZone);
-                        //$finishDate = new \DateTime($node->field_event_date[LANGUAGE_NONE][0]['value2'], $timeZone);
-                        $startDate = new \DateTime($node->field_event_date[LANGUAGE_NONE][0]['value']);
-                        $startDate->setTimezone($timeZone);
-                        $finishDate = new \DateTime($node->field_event_date[LANGUAGE_NONE][0]['value2']);
-                        $finishDate->setTimezone($timeZone);
-                        $customlink1 =
-                            ThemeHelper::createGoogleCalendarInsertUri($title, $details, $location, $startDate,
-                                                                       $finishDate);
-                    }
-                }
-                
-                $title = $node->title_field[LANGUAGE_NONE][0]['value'];
-                
-                $parentNid = false;
-                $parentTitle = false;
-                $parentIssueNumber = false;
-                if (isset($node->nodehierarchy_menu_links[0]['pnid']))
-                {
-                    $parentNid = $node->nodehierarchy_menu_links[0]['pnid'];
-                    $parentNode = node_load($parentNid);
-                    if ($parentNode)
-                    {
-                        //dpm($parentNode, "PARENT");
-                        $parentTitle = $parentNode->title_field[LANGUAGE_NONE][0]['value'];
-                        $parentIssueNumber = $parentNode->field_pubnumber[LANGUAGE_NONE][0]['value'];
-                    }
-                }
-                
-                $agoraNavBar = [
-                    '#theme'         => 'agoranav_article',
-                    '#backlink'      => url('<front>', []),
-                    '#issue'         => 'ISSUE #' . $parentIssueNumber,
-                    '#article_title' => $title,
-                ];
-                
-                if ($customlink1)
-                {
-                    $agoraNavBar['#customlink1'] = $customlink1;
-                }
-                
-                $vars['page']['content']['navbar'] = $agoraNavBar;
-            }
-        }
-    }
     
     /**
      * Add THS based on the area we are in
